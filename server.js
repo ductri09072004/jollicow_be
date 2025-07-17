915907f2d51e3d3660f6ffb67de4c3c59d7daeb8
BlueDuck <145476276+ductri09072004@users.noreply.github.com>
2025-07-17 13:15:49 +0700

    feat: 2

--- server.js
+++ server.js
@@ -24,10 +24,7 @@ app.set('trust proxy', true);
 app.use(roleFilter);
 
 // Middleware
-app.use(cors({
-  origin: "https://jollicow-client-production.up.railway.app",
-  credentials: true,
-}));
+app.use(cors());
 app.use(json());
 
 // Routes Staffs (không có middleware kiểm tra IP)
